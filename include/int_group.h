917cf651e4fb4a80a463a08dea01ed99eb4f51a0
“GabrielVScalese” <gabriel.scalese@hotmail.com>
2024-11-02 17:40:39 -0300

    feat: remove unnecessary funcs in the header

--- include/int_group.h
+++ include/int_group.h
@@ -10,28 +10,12 @@ typedef struct int_group {
 } int_group;
 
 /**
- * Mescla dois grupos de inteiros ordenados de forma a obter um grupo ordenado
- * @param a grupo de inteiros ordenados (crescente)
- * @param b grupo de inteiros ordenados (crescente)
- * @return grupo de inteiros ordenados contendo a e b
- */
-int_group *merge_two_groups(int_group *a, int_group *b);
-
-/**
  * Mescla um vetor de grupos de inteiros num unico grupo de inteiros, mantendo ordenacao
  * @param groups vetor de grupos de inteiros
  * @param group_quantity quantidade de grupos de inteiros no vetor
  * @return grupo unico de inteiros
  */
 int_group *merge_groups(int_group **groups, int group_quantity);
-
-/**
- * Algoritmo de ordenacao de grupo de inteiros baseado no paradigma "dividir-e-conquistar"
- * @param group grupo de inteiros a ser ordenado
- * @param start_pos posicao inicial do grupo de inteiros
- * @param end_pos posicao final do grupo de inteiros
- */
-void quick_sort(int_group *group, int start_pos, int end_pos);
 
 /**
  * Ordena um grupo de inteiros utilizando algoritmo quick sort
